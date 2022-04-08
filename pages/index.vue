feecc3cfe736139fbcd02fcdb0bb452a81b2f635
dalenysivilay <dalenysivilay@gmail.com>
2022-04-08 17:54:26 -0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NavBar />
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+
+export default {
+  name: 'IndexPage',
+  components: {
+    NavBar,
+  },
+};
+</script>
